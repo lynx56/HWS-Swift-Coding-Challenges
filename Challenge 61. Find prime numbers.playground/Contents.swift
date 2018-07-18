e02c11df2147e594422055d911409e27085b7227
Gulnaz <666lynx666@mail.ru>
2018-07-18 12:14:24 +0300

    Challenge 61. Split files

--- Challenge 61. Find prime numbers.playground/Contents.swift
+++ Challenge 61. Find prime numbers.playground/Contents.swift
@@ -6,35 +6,6 @@
 
 import Foundation
 
-extension Int{
-    func isPrime2()->Bool{
-        guard self > 1 && self != 2 else { return false }
-        
-        //if the number n is not prime, it means it can be reached by multiplying two factors, x and y. If both of those numbers were greater than the square root of n, then x * y would be greater than n, which is not possible. So, we can be sure that at least one of x or y is less than or equal to the square root of n.
-        let max = Int(ceil(sqrt(Double(self))))
-        
-        for i in 2..<max{
-            if self % i == 0{
-                return false
-            }
-        }
-        
-        return true
-    }
-    
-    func isPrime()->Bool{
-        guard self > 1 else { return false }
-        
-        for i in 2..<self{
-            if self % i == 0{
-                return false
-            }
-        }
-        
-        return true
-    }
-}
-
 var countOps = 50
 let queue = OperationQueue()
 
@@ -81,38 +52,18 @@ func getPrimesBySieveofEratosthenes(upTo: Int)->[Int]{
     return sieve.enumerated().compactMap { $0.element == false ? $0.offset : nil }
 }
 
-class FindPrimeOperation: AsyncOperation{
-    let lowerBound: Int
-    let upperBound: Int
-    var output: [Int]
-    
-    init(_ from: Int, to: Int){
-        self.lowerBound = from
-        self.upperBound = to
-        self.output = [Int]()
-    }
-    
-    override func main() {
-        for i in lowerBound..<upperBound{
-            if i.isPrime(){
-                output.append(i)
-            }
-        }
-        
-        self.state = .finished
-    }
-}
+
 
 getPrimes(upTo: 10)
 getPrimes(upTo: 11)
 getPrimes(upTo: 12)
 
 startClock()
-getPrimesBySieveofEratosthenes(upTo: 400)
+getPrimesBySieveofEratosthenes(upTo: 500)
 stopClock()
 
 startClock()
-getPrimes(upTo: 400)
+getPrimes(upTo: 500)
 stopClock()
 
 
